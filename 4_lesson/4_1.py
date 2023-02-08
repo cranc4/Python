8ff873f9466d422d47e1fc21e6ec8e6ace4e0801
Artem_S <cranc4568@gmail.com>
2023-02-08 22:38:21 +0300

    Hw4

--- 4_lesson/4_1.py
+++ 4_lesson/4_1.py
@@ -4,6 +4,9 @@
 # Количество повторов добавляется к
 # символам с помощью постфикса формата _n.
 
+# Input: a a a b c a a d c d d
+# Output: a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
+
 # string = input().split()
 
 # for i in range(len(string)):
