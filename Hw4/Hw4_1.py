c6e6edd56dffeadd969ecf76db71df22eeddad9c
Artem_S <cranc4568@gmail.com>
2023-02-12 13:43:51 +0300

    Hw5

--- Hw4/Hw4_1.py
+++ Hw4/Hw4_1.py
@@ -16,3 +16,11 @@ print(*list_3)
 list_3.sort()
 if list_3 == []:
     print("Нет общих чисел")
+
+# -------------------- 2 вариант
+
+    n, m = input().split()
+first = [int(i) for i in input().split()]
+second = [int(j) for j in input().split()]
+
+print(*sorted(set(first).intersection(second)))
